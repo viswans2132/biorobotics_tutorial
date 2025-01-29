a924ca8f6541c65f97b16f64737300b822372881
viswans2132 <viswans2132@gmail.com>
2025-01-29 01:24:18 +0100

    Testing with box.xacro

--- launch/test.launch.py
+++ launch/test.launch.py
@@ -13,26 +13,18 @@ import xacro
 
 def generate_launch_description():
     namespace = 'robot'
-    box_name = 'ground'
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
     pkg_robot = get_package_share_directory('gazebo_urdf_tutorial')
-    xacro_file = os.path.join(pkg_robot, 'models', 'quadruped.xacro')
+    xacro_file = os.path.join(pkg_robot, 'models', 'box.xacro')
     assert os.path.exists(xacro_file), "The file path seems to be wrong: "+str(xacro_file)
-    box_file = os.path.join(pkg_robot, 'models', 'box.xacro')
-    assert os.path.exists(box_file), "The file path seems to be wrong: "+str(box_file)
     # world_file = os.path.join(pkg_gazebo_ros, 'worlds', 'empty.world')
     # assert os.path.exists(world_file), "The file path seems to be wrong: "+str(world_file)
 
-    robot_desc_config = xacro.process_file(xacro_file,
-        mappings={'namespace':namespace})
+    robot_desc_config = xacro.process_file(xacro_file)
     robot_desc = robot_desc_config.toxml()
 
 
-
-    box_desc_config = xacro.process_file(box_file,
-        mappings={'box_name':box_name, 'mass': '100.0'})
-    box_desc = box_desc_config.toxml()
 
 
 
@@ -53,7 +45,7 @@ def generate_launch_description():
             description='SDF world file'),
         gazebo,
         Node(
-            package='gazebo_urdf_tutorial',
+            package='biorobotics_tutorial',
             executable='spawn_bot',
             name='robot_spawner',
             arguments=[robot_desc, '0.0', '0.0', '0.5'],
@@ -69,11 +61,4 @@ def generate_launch_description():
                 }],
                 output='screen'
             ),
-        # Node(
-        #     package='gazebo_urdf_tutorial',
-        #     executable='quadruped_controller',
-        #     name='quadruped_controller',
-        #     arguments=[namespace,'-0.5', '-1.0', '0.0', '0.0'],
-        #     output='screen'
-        # ),        
         ])
