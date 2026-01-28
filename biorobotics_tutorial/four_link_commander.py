f62cf5b32f34a1a9f5bc4c5af82694532f25b978
viswans2132 <viswans2132@gmail.com>
2026-01-28 09:46:41 +0100

    Four link commander added

--- biorobotics_tutorial/four_link_commander.py
+++ biorobotics_tutorial/four_link_commander.py
@@ -1,69 +1,95 @@
 #!/usr/bin/python3
-# Import necessary modules
 import os
 import sys
+
 import rclpy
 from rclpy.duration import Duration
 from rclpy.action import ActionClient
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy, QoSDurabilityPolicy
+
+from control_msgs.action import FollowJointTrajectory
+from trajectory_msgs.msg import JointTrajectoryPoint
 from sensor_msgs.msg import JointState
-from std_msgs.msg import Float64MultiArray
+
 import numpy as np
 
-# Define a class for the JointCommander node
-class JointCommander(Node):
+
+class TwoLinkControllerClient(Node):
     def __init__(self, namespace=''):
-        # Initialize the node with the name 'joint_commander'
-        super().__init__('joint_commander')
-        
-        # Define the QoS profile for the publisher
+        super().__init__('two_link_control_action_client')
         qos_profile = QoSProfile(
             reliability=QoSReliabilityPolicy.BEST_EFFORT,
             durability=QoSDurabilityPolicy.VOLATILE,
             history=QoSHistoryPolicy.KEEP_LAST,
             depth=1
         )
+        self._action_client = ActionClient(self, FollowJointTrajectory, '/joint_trajectory_controller/follow_joint_trajectory')
+        self._state_sub = self.create_subscription(JointState, '/joint_states', self.state_callback, qos_profile)
+        self._namespace = namespace
+        self._cur_position = np.array([0.0, 0.0])
 
-        # Create a publisher for the position controller commands
-        self._joint_pub = self.create_publisher(Float64MultiArray, '/position_controller/commands', qos_profile)
+    def send_goal(self, angle):
+        goal_msg = FollowJointTrajectory.Goal()
 
-    def pub_goal(self, ang):
-        """
-        Publish the goal angles to the position controller.
-        """
-        goal_msg = Float64MultiArray()
-        goal_msg.data = [ang, ang]
+        joint_name = [self._namespace + "/link_joint1", self._namespace + "/link_joint2", self._namespace + "/link_joint11", self._namespace + "/link_joint22"]
 
-        # Publish the goal message
-        self._joint_pub.publish(goal_msg)
-        self.get_logger().info(f"Sent goal: Joint 1 = {ang}, Joint 2 = {ang}")
+        points = []
+        point = JointTrajectoryPoint()
+        point.time_from_start = Duration(seconds=1, nanoseconds=0).to_msg()
+        point.positions = [angle, angle, angle, angle]
+
+        points.append(point)
+
+        goal_msg.goal_time_tolerance = Duration(seconds=1, nanoseconds=0).to_msg()
+        goal_msg.trajectory.joint_names = joint_name
+        goal_msg.trajectory.points = points
+
+        self._action_client.wait_for_server()
+        self._send_goal_future = self._action_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback)
+
+        self._send_goal_future.add_done_callback(self.goal_response_callback)
+    
+    def goal_response_callback(self, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().info('Goal rejected :(')
+            return
+
+        self.get_logger().info('Goal accepted :)')
+
+        self._get_result_future = goal_handle.get_result_async()
+        self._get_result_future.add_done_callback(self.get_result_callback)
+    
+    def get_result_callback(self, future):
+        result = future.result().result
+        self.get_logger().info('Result: '+str(result))
+        raise SystemExit
+
+    def feedback_callback(self, feedback_msg):
+        feedback = feedback_msg.feedback
+        # self.get_logger().info('Received feedback:'+str(feedback))
+
+    def state_callback(self, state_msg):
+        self._cur_position = state_msg.position
+        # print(state_msg.position)
+
+    
 
 def main(args=None):
-    """
-    Main function to initialize the ROS 2 node and start the controller.
-    """
-    # Initialize the ROS 2 Python client library
+    
     rclpy.init()
 
-    # Get the goal angle from the command line arguments
+    namespace = "robot"
+    action_client = TwoLinkControllerClient(namespace)
     angle = float(sys.argv[1])
-
-    # Create an instance of the JointCommander
-    joint_commander = JointCommander()
-    joint_commander.pub_goal(angle)
-
+    future = action_client.send_goal(angle)
     try:
-        # Spin the node to keep it active
-        rclpy.spin(joint_commander)
+        rclpy.spin(action_client)
     except SystemExit:
-        # Log a message when shutting down
         rclpy.logging.get_logger('two_link_controller_client').info('Shutting down the two_link_controller_client')
-    
-    # Destroy the node explicitly
-    joint_commander.destroy_node()
+    action_client.destroy_node()
 
-    # Shutdown the ROS 2 Python client library
     rclpy.shutdown()
 
 if __name__ == '__main__':
